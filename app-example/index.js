add721badb9432096c5bde007e617f03b9ad7ca3
IvanSlivenko <ivan8822@ukr.net>
2025-09-21 19:43:21 +0300

    21-09-2025-19-43

---
+++ app-example/index.js
@@ -0,0 +1,16 @@
+const { createServer } = require("node:http");
+
+const port = 3000;
+
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World");
+});
+
+server.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
+
+
